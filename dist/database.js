e5dcad180cef5f1c7b39373ed9f4592696fad7a0
David Millan <davidhastt@gmail.com>
2023-10-30 01:30:04 -0600

    Primer commit

---
+++ dist/database.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
+const pg_1 = require("pg");
+//import {config} from 'dotenv';
+//config();
+//si se desarrolla en entorno local activa este bloque de codigo
+exports.pool = new pg_1.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    password: 'davitzoL18',
+    database: 'menusdb',
+    port: 5432,
+    //ssl:true
+});
+//external url de render
+/*
+export const pool = new Pool({
+    user:'davidmillan',
+    host:'dpg-ckuhmr237rbc738ccnq0-a',
+    password: 'D3zujgTZLkwBCPSowXCPzLBqFAEi27m8',
+    database: 'menusdb',
+    port: 5432
+});
+*/
---
+++ src/database.ts
@@ -0,0 +1,30 @@
+import { Pool } from "pg";
+//import {config} from 'dotenv';
+//config();
+
+
+//si se desarrolla en entorno local activa este bloque de codigo
+export const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    password: 'davitzoL18',
+    database: 'menusdb',
+    port: 5432,
+    //ssl:true
+});
+
+
+
+//external url de render
+/*
+export const pool = new Pool({
+    user:'davidmillan',
+    host:'dpg-ckuhmr237rbc738ccnq0-a',
+    password: 'D3zujgTZLkwBCPSowXCPzLBqFAEi27m8',
+    database: 'menusdb',
+    port: 5432
+});
+*/
+
+
+
